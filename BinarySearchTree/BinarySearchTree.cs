a5d1accf2882e5f6356c58fac72f7036e8c2c5f3
Don-Uherka <uherkadon@yahoo.com>
2020-10-08 08:58:07 -0500

    i finished adding things to the tree almost finished the search but had to stop due to time contraints on other projects

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        //variables
+        public Node treeBase;
+       
+
+        //contructor
+        public BinarySearchTree()
+        {
+            treeBase = null;
+            
+        }
+
+        //methods
+        public void Add(int valueToAdd)
+        {
+            Node nodeToAdd = new Node(valueToAdd);
+            if(treeBase == null)
+            {
+                treeBase = nodeToAdd;
+            }
+            else
+            {
+                Node currentNode = treeBase;
+                while(true)
+                {
+                    //currentNode = currentNode.data;
+                    if(valueToAdd <= currentNode.data)
+                    {
+                        if (currentNode.left != null)
+                        {
+                            currentNode = currentNode.left;
+                        }
+                        else
+                        {
+                            currentNode.left = nodeToAdd;
+                            break;
+                        }
+                    }
+                    else 
+                    {
+                        if(valueToAdd >= currentNode.data)
+                        {
+                            if(currentNode.right != null)
+                            {
+                                currentNode = currentNode.right;
+                            }
+                            else
+                            {
+                                currentNode.right = nodeToAdd;
+                                break;
+                            }
+                        }
+                        //same for the right side
+                    }
+                }
+                
+            }
+        }
+        public bool Search(int numberToSearch)
+        {
+            Node currentNode = treeBase;
+            while (true)
+            {
+                //currentNode = currentNode.data;
+                if (numberToSearch <= currentNode.data)
+                {
+                    if (currentNode.left != null)
+                    {
+                        currentNode = currentNode.left;
+                    }
+                    else
+                    {
+                        currentNode.left = numberToSearch;
+                        break;
+                    }
+                }
+                else
+                {
+                    if (numberToSearch >= currentNode.data)
+                    {
+                        if (currentNode.right != null)
+                        {
+                            = currentNode.right;
+                        }
+                        else
+                        {
+                            currentNode.right = nodeToAdd;
+                            break;
+                        }
+                    }
+                    //same for the right side
+                }
+
+
+
+                //Similar currentNode strategy
+                //Reuse traversal logic moving down one branch or the other
+                //First have to check whether currentnode.data == numbertosearch - return true
+                //if you ever find a null branch, instead of adding the item, return false
+
+                return false;
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        //variables
+        public int data;
+        public Node left;
+        public Node right;
+        int nodeOne = 85;
+        int nodeTwo = 156;
+        int nodeThree = 43;
+        int nodeFour = 102;
+
+        //constructor
+        public Node(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+
+        //methods
+    }
+}
